629b8d01cbb9d5453d991290e6014de625878cc0
Riley Barabash <admin@rileybarabash.com>
2022-11-26 21:09:42 -0700

    feat: button
    Added SDButton.

---
+++ sources/components/button.swift
@@ -0,0 +1,68 @@
+
+//
+//  SDKit: Button
+//  Developed by SPLIT Designs
+//
+
+//  MARK: - Imports
+
+import SwiftUI
+
+//
+//
+
+//  MARK: - Structures
+
+/// A button containing some text.
+///
+@available ( iOS 16.0, * )
+public struct SDButton < Content: View > : View {
+	
+	/// Access to the `SDDefaults` configuration.
+	///
+	@Environment ( \ .defaults ) private var defaults
+	
+	/// The content to display.
+	///
+	private let content: ( ) -> Content
+	
+	/// The action to run when the button is pressed.
+	///
+	private let action: ( ) -> Void
+	
+	/// A press interaction containing some styled text.
+	///
+	public var body: some View {
+		
+		SDPressInteraction ( animation: defaults.animations.expoOut ( duration: 0.5 ) ) { response in
+			
+			content ( )
+				.font ( defaults.fonts.monospaced ( ) )
+				.padding ( 12.0 )
+				.padding ( .horizontal, 6.0 )
+				.background ( SDBackdropFilter ( tint: response.isPressed ? defaults.colors.accent.auto.opacity ( 0.5 ) : nil ) )
+				.relativeCornerStyle ( subject: 0.0, relative: 64.0 )
+				.shadow ( color: defaults.shadows.color, radius: defaults.shadows.radius )
+				.task ( id: response.isPressed ) { if !response.isPressed && response.isContained { action ( ) } }
+			
+		}
+		
+	}
+	
+	/// Creates a ``SDButton`` from some text and an action.
+	///
+	/// - Parameters:
+	///   - content: The content to display.
+	///   - action: The action to run when the button is tapped.
+	///
+	public init ( @ViewBuilder content: @escaping ( ) -> Content, perform action: @escaping ( ) -> Void ) {
+		
+		self.content = content
+		self.action = action
+		
+	}
+	
+}
+
+//
+//
